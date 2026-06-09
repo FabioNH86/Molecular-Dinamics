f1d0329d26dda11ce94316605e9a87a4dbf1c69b
FabioNH86 <fabionoriega86@icloud.com>
2026-06-09 09:35:25 -0600

    COntinuación simulaciones

--- ejecutar_simulacion.py
+++ ejecutar_simulacion.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from funciones import crear_primer_frame_homopolimero, correr_simulacion_homoplimero
+from funciones import crear_primer_frame_homopolimero, correr_simulacion_homoplimero, continue_sim_from_gsd
 
 """
 Script preparado para recibir parámetros desde Bash.
@@ -27,34 +27,57 @@ if not os.path.exists(ruta_base):
 
 print(f"\n🧪 [PROCESO AISLADO] Iniciando: T={temperatura}, eps_SP={eps}, Monómeros/Cadena={n_monomeros}")
 
-try:
+try: 
     # Moverse a la ruta de resultados antes de inicializar HOOMD
     os.chdir(os.path.join(directorio_original, ruta_base))
-    
+        
     # Parámetros de tiempo
-    pasos_equil = int(2.5e5)
+    pasos_equil = int(2e5)
     pasos_muestreo = int(1e6)
-    
-    # Generar Snapshot
-    snapshot = crear_primer_frame_homopolimero(
-        densidad_goticula=0.6,
-        aspect_ratio=1,
-        concentracion_porcentual_monomeros=1.0,
-        n_monomeros=n_monomeros_totales,
-        monomeros_en_polimero=n_monomeros
-    )
-    
-    # Ejecutar en HOOMD
-    correr_simulacion_homoplimero(
-        snapshot=snapshot,  
-        temp=temperatura,
-        equilibracion=pasos_equil, 
-        muestreo=pasos_muestreo,
-        eps_SP=eps,
-        mon_cadena=n_monomeros,
-        aspect_ratio=2.0
-    )
-    print(f"✅ ¡Simulación finalizada con éxito para T={temperatura}, N={n_monomeros}!")
+
+    # Agregamos los nombres de los archivos que ya deberían estar creados
+    file_id = f"Poly-Solv_T{temperatura:.2f}_epsSP{eps:.2f}"
+    fn_gsd = f"{file_id}_monom_{n_monomeros}.gsd"
+    print(f"Buscando: {fn_gsd}")
+
+    # Revisamos si ya están creados los archivos 
+    if os.path.exists(fn_gsd):
+        print(f"🔄 Archivo previo '{fn_gsd}' detectado. Intentando reanudar...")
+
+
+        # Llamamos la función que continúa la simulación 
+        continue_sim_from_gsd(
+            archivo_gsd=fn_gsd,
+            temp=temperatura,
+            muestreo=pasos_muestreo,
+            eps_SP=eps,
+            mon_cadena=n_monomeros,
+            aspect_ratio=2.0
+        )
+        print(f"✅ ¡Simulación REANUDADA y finalizada con éxito para T={temperatura}, N={n_monomeros}!")
+
+    else:
+        print("🆕 No se encontró GSD previo. Iniciando simulación desde cero...")
+        # Generar Snapshot
+        snapshot = crear_primer_frame_homopolimero(
+            densidad_goticula=0.6,
+            aspect_ratio=1,
+            concentracion_porcentual_monomeros=1.0,
+            n_monomeros=n_monomeros_totales,
+            monomeros_en_polimero=n_monomeros
+        )
+        
+        # Ejecutar en HOOMD
+        correr_simulacion_homoplimero(
+            snapshot=snapshot,  
+            temp=temperatura,
+            equilibracion=pasos_equil, 
+            muestreo=pasos_muestreo,
+            eps_SP=eps,
+            mon_cadena=n_monomeros,
+            aspect_ratio=2.0
+        )
+        print(f"✅ ¡Simulación finalizada con éxito para T={temperatura}, N={n_monomeros}!")
 
 except Exception as e:
     print(f"❌ Error crítico en este proceso: {e}")
